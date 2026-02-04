18e0eec31e4ad95f4f2c407d10ad71bb325b7d67
Oliver Steiner <github@jsemolik.dev>
2026-02-04 13:36:53 +0100

    BRO FINALLY

--- Engine/Runtime/Include/HorseEngine/Core/Logging.h
+++ Engine/Runtime/Include/HorseEngine/Core/Logging.h
@@ -1,45 +1,51 @@
 #pragma once
 
 #include "HorseEngine/Core.h"
-#include <spdlog/spdlog.h>
 #include <memory>
+#include <spdlog/spdlog.h>
+
 
 namespace Horse {
 
-enum class LogChannel {
-    Core,
-    Render,
-    Asset,
-    Script
-};
+enum class LogChannel { Core, Render, Asset, Script };
 
 class Logger {
 public:
-    static void Initialize();
-    static void Shutdown();
-    
-    static std::shared_ptr<spdlog::logger> GetLogger(LogChannel channel);
-    
-    template<typename... Args>
-    static void LogCore(spdlog::level::level_enum level, Args&&... args) {
-        GetLogger(LogChannel::Core)->log(level, std::forward<Args>(args)...);
-    }
-    
-    template<typename... Args>
-    static void LogRender(spdlog::level::level_enum level, Args&&... args) {
-        GetLogger(LogChannel::Render)->log(level, std::forward<Args>(args)...);
-    }
+  static void Initialize();
+  static void Shutdown();
+
+  static std::shared_ptr<spdlog::logger> GetLogger(LogChannel channel);
+
+  template <typename... Args>
+  static void LogCore(spdlog::level::level_enum level,
+                      spdlog::format_string_t<Args...> fmt, Args &&...args) {
+    GetLogger(LogChannel::Core)->log(level, fmt, std::forward<Args>(args)...);
+  }
+
+  template <typename... Args>
+  static void LogRender(spdlog::level::level_enum level,
+                        spdlog::format_string_t<Args...> fmt, Args &&...args) {
+    GetLogger(LogChannel::Render)->log(level, fmt, std::forward<Args>(args)...);
+  }
 };
 
 } // namespace Horse
 
 // Convenience macros
-#define HORSE_LOG_CORE_TRACE(...)    Horse::Logger::LogCore(spdlog::level::trace, __VA_ARGS__)
-#define HORSE_LOG_CORE_INFO(...)     Horse::Logger::LogCore(spdlog::level::info, __VA_ARGS__)
-#define HORSE_LOG_CORE_WARN(...)     Horse::Logger::LogCore(spdlog::level::warn, __VA_ARGS__)
-#define HORSE_LOG_CORE_ERROR(...)    Horse::Logger::LogCore(spdlog::level::err, __VA_ARGS__)
+#define HORSE_LOG_CORE_TRACE(...)                                              \
+  Horse::Logger::LogCore(spdlog::level::trace, __VA_ARGS__)
+#define HORSE_LOG_CORE_INFO(...)                                               \
+  Horse::Logger::LogCore(spdlog::level::info, __VA_ARGS__)
+#define HORSE_LOG_CORE_WARN(...)                                               \
+  Horse::Logger::LogCore(spdlog::level::warn, __VA_ARGS__)
+#define HORSE_LOG_CORE_ERROR(...)                                              \
+  Horse::Logger::LogCore(spdlog::level::err, __VA_ARGS__)
 
-#define HORSE_LOG_RENDER_TRACE(...)  Horse::Logger::LogRender(spdlog::level::trace, __VA_ARGS__)
-#define HORSE_LOG_RENDER_INFO(...)   Horse::Logger::LogRender(spdlog::level::info, __VA_ARGS__)
-#define HORSE_LOG_RENDER_WARN(...)   Horse::Logger::LogRender(spdlog::level::warn, __VA_ARGS__)
-#define HORSE_LOG_RENDER_ERROR(...)  Horse::Logger::LogRender(spdlog::level::err, __VA_ARGS__)
+#define HORSE_LOG_RENDER_TRACE(...)                                            \
+  Horse::Logger::LogRender(spdlog::level::trace, __VA_ARGS__)
+#define HORSE_LOG_RENDER_INFO(...)                                             \
+  Horse::Logger::LogRender(spdlog::level::info, __VA_ARGS__)
+#define HORSE_LOG_RENDER_WARN(...)                                             \
+  Horse::Logger::LogRender(spdlog::level::warn, __VA_ARGS__)
+#define HORSE_LOG_RENDER_ERROR(...)                                            \
+  Horse::Logger::LogRender(spdlog::level::err, __VA_ARGS__)
--- Engine/Runtime/Include/HorseEngine/Platform/Win32Window.h
+++ Engine/Runtime/Include/HorseEngine/Platform/Win32Window.h
@@ -4,7 +4,6 @@
 #include <string>
 #include <functional>
 
-#define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
 namespace Horse {
